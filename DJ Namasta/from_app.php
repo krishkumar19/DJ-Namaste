dd68273495f0be7303c9f4a96532c9c2499c5b49
Krish <sahilsingh19feb@gmail.com>
2026-05-09 05:37:00 +0530

    update

---
+++ DJ Namasta/from_app.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $number = $_POST["number"];
+    $message = $_POST["message"];
+   
+	// Database connection
+	$conn = new mysqli('localhost','root','','kk');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(name, email, number, message) values(?, ?, ?, ?)");
+		$stmt->bind_param("ssis", $name, $email, $number, $message);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
